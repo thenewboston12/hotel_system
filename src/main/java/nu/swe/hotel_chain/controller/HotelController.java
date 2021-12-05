67d7a5c96aaa20f6d90c66e87e0efcf6fd7186f3
PikaBeka <beka.kalmahanbet@gmail.com>
2021-12-06 00:23:42 +0600

    clean architecture structure

---
+++ src/main/java/nu/swe/hotel_chain/controller/HotelController.java
@@ -0,0 +1,18 @@
+package nu.swe.hotel_chain.controller;
+
+import nu.swe.hotel_chain.models.Hotel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/hotel")
+public class HotelController {
+
+//    @GetMapping
+//    public List<Hotel> getHotels(){
+//
+//    }
+}
