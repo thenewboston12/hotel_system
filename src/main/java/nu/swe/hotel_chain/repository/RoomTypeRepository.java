1542c5cb87c749728dd0f3c4104725a48ca9b5b4
PikaBeka <beka.kalmahanbet@gmail.com>
2021-12-09 08:08:34 +0600

    Corrected get requests for room api

--- src/main/java/nu/swe/hotel_chain/repository/RoomTypeRepository.java
+++ src/main/java/nu/swe/hotel_chain/repository/RoomTypeRepository.java
@@ -3,8 +3,13 @@ package nu.swe.hotel_chain.repository;
 import nu.swe.hotel_chain.models.RoomType;
 import nu.swe.hotel_chain.models.RoomTypeId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoomTypeRepository extends JpaRepository<RoomType, RoomTypeId> {
+    @Query(value = "select RT from RoomType as RT where RT.r_type = ?1")
+    List<RoomType> findByR_type(String r_type);
 }
--- src/main/java/nu/swe/hotel_chain/service/RoomService.java
+++ src/main/java/nu/swe/hotel_chain/service/RoomService.java
@@ -1,20 +1,31 @@
 package nu.swe.hotel_chain.service;
 
+import nu.swe.hotel_chain.exceptions.IllegalIdException;
+import nu.swe.hotel_chain.models.Hotel;
 import nu.swe.hotel_chain.models.Room;
+import nu.swe.hotel_chain.models.RoomType;
+import nu.swe.hotel_chain.models.RoomTypeId;
+import nu.swe.hotel_chain.repository.HotelRepository;
 import nu.swe.hotel_chain.repository.RoomRepository;
+import nu.swe.hotel_chain.repository.RoomTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
     private final RoomRepository roomRepository;
+    private final HotelRepository hotelRepository;
+    private final RoomTypeRepository roomTypeRepository;
 
     @Autowired
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, HotelRepository hotelRepository, RoomTypeRepository roomTypeRepository) {
         this.roomRepository = roomRepository;
+        this.hotelRepository = hotelRepository;
+        this.roomTypeRepository = roomTypeRepository;
     }
 
     public List<Room> getRooms(){
@@ -22,19 +33,35 @@ public class RoomService {
     }
 
     public List<Room> getRoomByHotelId(String hotel_id){
+        Optional<Hotel> roomOptional = this.hotelRepository.findById(hotel_id);
+        if (!roomOptional.isPresent()){
+            throw new IllegalIdException("There is no hotel with id " + hotel_id);
+        }
         return this.roomRepository.findRoomByHotel_id(hotel_id);
     }
 
     public List<Room> getRoomByRoomType(String r_type) {
+        List<RoomType> roomTypeList = this.roomTypeRepository.findByR_type(r_type);
+        if(roomTypeList.size() == 0){
+            throw new IllegalIdException("There is no room types with type " + r_type);
+        }
         return this.roomRepository.findRoomByR_type(r_type);
     }
 
     public List<Room> getRoomByHotelIdAndR_Type(String hotel_id, String r_type) {
+        Optional<RoomType> roomTypeOptional = this.roomTypeRepository.findById(new RoomTypeId(hotel_id, r_type));
+        if(!roomTypeOptional.isPresent()){
+            throw new IllegalIdException("There is no rooms with hotel id " + hotel_id + " and r_type " + r_type);
+        }
         return this.roomRepository.findRoomByHotel_idAndR_type(hotel_id, r_type);
     }
 
     public List<Room> getAvailableRoomBetweenDatesAndByHotel_IdAndR_Type(String hotel_id, String r_type,
                                                                          LocalDate check_in_date, LocalDate check_out_date) {
+        Optional<RoomType> roomTypeOptional = this.roomTypeRepository.findById(new RoomTypeId(hotel_id, r_type));
+        if(!roomTypeOptional.isPresent()){
+            throw new IllegalIdException("There is no rooms with hotel id " + hotel_id + " and r_type " + r_type);
+        }
         return this.roomRepository.findAvailableRoomsInHotelWithR_Type(hotel_id, r_type, check_in_date, check_out_date);
     }
 }
