752fdf4e62b0d9e90f625fd972f05d751372a1a1
PikaBeka <beka.kalmahanbet@gmail.com>
2021-12-09 19:45:42 +0600

    Added cross origin

---
+++ src/main/java/nu/swe/hotel_chain/config/WebConfig.java
@@ -0,0 +1,13 @@
+package nu.swe.hotel_chain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
